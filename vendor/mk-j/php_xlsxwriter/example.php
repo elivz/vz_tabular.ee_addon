6179e59f0c1015e9d41deed65b535fc29835b386
Eli Van Zoeren <eli@elivz.com>
2019-06-16 13:39:47 -0400

    Update xlsxwriter dependency

--- vendor/mk-j/php_xlsxwriter/example.php
+++ vendor/mk-j/php_xlsxwriter/example.php
@@ -13,10 +13,11 @@ header('Pragma: public');
 
 $rows = array(
     array('2003','1','-50.5','2010-01-01 23:00:00','2012-12-31 23:00:00'),
-    array('2003','=B2', '23.5','2010-01-01 00:00:00','2012-12-31 00:00:00'),
+    array('2003','=B1', '23.5','2010-01-01 00:00:00','2012-12-31 00:00:00'),
 );
+
 $writer = new XLSXWriter();
-$writer->setAuthor('Some Author');
+$writer->setAuthor('Some Author'); 
 foreach($rows as $row)
 	$writer->writeSheetRow('Sheet1', $row);
 $writer->writeToStdOut();
